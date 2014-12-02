79b95a83b8ddebd10c0b10fe13d4cd25a26539d4
Michael R. Fisher <mfisher31@gmail.com>
2014-12-01 23:40:24 -0500

    Updating jucer files

--- standalone/JuceLibraryCode/JuceHeader.h
+++ standalone/JuceLibraryCode/JuceHeader.h
@@ -40,11 +40,13 @@
  using namespace juce;
 #endif
 
+#if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
     const char* const  projectName    = "KSP1 Standalone";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
 }
+#endif
 
 #endif   // __APPHEADERFILE_GPA4HQ__
